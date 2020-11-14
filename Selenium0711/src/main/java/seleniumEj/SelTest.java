dcad69f8bcedad3a9fbb74a5322e9551c5da2a33
Andrea Torres <atorres@cellforce.com>
2020-11-14 09:49:42 -0500

    First Commit

---
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -0,0 +1,19 @@
+package seleniumEj;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTest {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator;	// Traer el path del proyecto y el separator es el '/' y con esto seteamos el path de nuestro driver
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver"); 	// Identificando el ejecutable para el driver
+		WebDriver driver = new ChromeDriver();	// Creacion en tiempo de ejecucion de nuestro ChromeDriver
+		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");	// Abriendo el Chrome con la URL especificada
+	}
+
+}
